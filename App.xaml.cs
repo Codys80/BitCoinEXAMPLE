1da8f3ab8fd5355986ebf867571fa3612cbc1434
CodyS80 <bartolosemolo@gmail.com>
2024-11-05 12:47:06 +0100

    Dodano projekt

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BitcoinEXAMPLE
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BitcoinEXAMPLE
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
